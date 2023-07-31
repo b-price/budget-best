c3e1fa5be9147336efaa4cf808ebbd333f3178c5
AlexGallamoza <112433660+AlexGallamoza@users.noreply.github.com>
2023-07-31 18:33:44 -0400

    Able to subtract expenses from income

--- src/components/IncomeCard.js
+++ src/components/IncomeCard.js
@@ -4,6 +4,16 @@ import BudgetCard from "./BudgetCard"
 export default function IncomeCard(props) {
     const { income } = useBudgets()
     const amount = income.reduce((total, income) => total + income.amount, 0)
+    const expensesAmount = expenses.reduce((total, expense) => total + expense.amount, 0);
+    // Calculate the remaining balance by subtracting expenses from income
+    const remainingBalance = incomeAmount - expensesAmount;
 
-    return <BudgetCard amount={amount} name="Income" gray hideButtons incomeCard {...props} />
+    return <BudgetCard 
+    amount={incomeAmount} 
+    name="Income" 
+    remainingBalance={remainingBalance}
+    gray 
+    hideButtons 
+    incomeCard 
+    {...props} />
 }
