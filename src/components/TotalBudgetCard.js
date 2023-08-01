b13f8b240afc93de3bc899574b779dfe1e5816a5
Katherine Lim <katherinerlim@gmail.com>
2023-08-01 15:24:16 -0400

    added income to max in total card

--- src/components/TotalBudgetCard.js
+++ src/components/TotalBudgetCard.js
@@ -2,12 +2,14 @@ import { useBudgets } from "../contexts/BudgetsContext"
 import BudgetCard from "./BudgetCard"
 
 export default function TotalBudgetCard() {
-  const { expenses, budgets } = useBudgets()
+  const { expenses, budgets, income } = useBudgets()
   const amount = expenses.reduce((total, expense) => total + expense.amount, 0)
-  const max = budgets.reduce((total, budget) => total + budget.max, 0)
+                 
+  const max = budgets.reduce((total, budget) => total + budget.max, 0) +
+              income.reduce((total, income) => total + income.amount, 0)
   
   // if there are no expenses/budgets do not show the total
   if (max === 0) return null
 
-  return <BudgetCard amount={amount} name="Total" gray max={max} hideButtons />
+  return <BudgetCard amount={amount} name="Total Expenses" gray max={max} hideButtons />
 }
