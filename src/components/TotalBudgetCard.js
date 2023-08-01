97b452c6c59fa70d2e032316a1f5dd1153f9db44
Katherine Lim <katherinerlim@gmail.com>
2023-08-01 15:26:40 -0400

    added comments to total card

--- src/components/TotalBudgetCard.js
+++ src/components/TotalBudgetCard.js
@@ -3,8 +3,9 @@ import BudgetCard from "./BudgetCard"
 
 export default function TotalBudgetCard() {
   const { expenses, budgets, income } = useBudgets()
+  // total of all expenses
   const amount = expenses.reduce((total, expense) => total + expense.amount, 0)
-                 
+  // account for income and all budgets in the max amount budgeted
   const max = budgets.reduce((total, budget) => total + budget.max, 0) +
               income.reduce((total, income) => total + income.amount, 0)
   
